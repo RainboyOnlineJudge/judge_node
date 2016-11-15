4d1d73a9189f389b25149386b36cb08ff2155855
Rainboy <597872644@qq.com>
2016-11-15 20:58:02 +0800

    [fix]test_seccomp

--- test2/test.js
+++ test2/test.js
@@ -4,5 +4,5 @@ var integration = require('./test_integration.js');
 var seccomp = require('./test_seccomp.js')
 
 integration();
-//seccomp();
+seccomp();
 
--- test2/test_seccomp.js
+++ test2/test_seccomp.js
@@ -1,6 +1,6 @@
 var should = require('should')
 var join = require('path').join;
-var judgerSync = require('../index.js').judgerSync;
+var judge = require('../father.js');
 var _judger = require('../index.js').flag;
 
 var base = require('./base.js');
@@ -34,84 +34,103 @@ var workspace = join(process.cwd(),'testcase/integration');
 
 function seccomp(){
     describe('test_fork',function(){
-        it('fork.c',function(){
+        it('fork.c',function(done){
+            this.timeout(15000)
             var config = JSON.parse( JSON.stringify(inte_config));
             config["max_memory"] = 1024 * 1024 * 1024
-            config['exe_path'] = _compile_c( join(t_path,'fork.c'));
-            config.output_path = config.error_path = _output_path();
-
-            // without seccomp
-            var res = judgerSync(config);
-            res.result.should.eql(_judger.SUCCESS);
-
-            // with general seccomp
-            config["seccomp_rule_name"] = "general"
-            res = judgerSync(config);
-            res.result.should.eql(_judger.RUNTIME_ERROR);
-            res.signal.should.eql(31);
-
-            // with c_cpp seccomp
-            config["seccomp_rule_name"] = "c_cpp"
-            res = judgerSync(config);
-            res.result.should.eql(_judger.RUNTIME_ERROR);
-            res.signal.should.eql(31);
+            
+            _compile_c( join(t_path,'fork.c'),function(exe_path){
+                config['exe_path'] = exe_path
+                config.output_path = config.error_path = _output_path();
+                judge(config,function(res){
+                    // without seccomp
+                    res.result.should.eql(_judger.SUCCESS);
+                    // with general seccomp
+                    config["seccomp_rule_name"] = "general"
+                    judge(config,function(res){
+                        // with general seccomp
+                        res.result.should.eql(_judger.RUNTIME_ERROR);
+                        res.signal.should.eql(31);
+                        config["seccomp_rule_name"] = "c_cpp"
+                        judge(config,function(res){
+                            // with c_cpp seccomp
+                            res.result.should.eql(_judger.RUNTIME_ERROR);
+                            res.signal.should.eql(31);
+                            done()
+                        })
+                    })
+                })
+            })
 
         })
     })
 
     describe('test_execve',function(){
-        it('execve.c',function(){
+        it('execve.c',function(done){
+            this.timeout(15000)
             var config = JSON.parse( JSON.stringify(inte_config));
             config["max_memory"] = 1024 * 1024 * 1024
-            config['exe_path'] = _compile_c( join(t_path,'execve.c'));
-            config.output_path = config.error_path = _output_path();
 
-            // without seccomp
-            var res = judgerSync(config);
-            var out = "Helloworld\n";
-            res.result.should.eql(_judger.SUCCESS);
-            out.should.eql( output_content(config.output_path))
+            _compile_c( join(t_path,'execve.c'),function(exe_path){
+                config['exe_path'] = exe_path
+                config.output_path = config.error_path = _output_path();
+                judge(config,function(res){
+                    //without seccomp
+                    var out = "Helloworld\n";
+                    res.result.should.eql(_judger.SUCCESS);
+                    out.should.eql( output_content(config.output_path))
 
-            // with general seccomp
-            config["seccomp_rule_name"] = "general"
-            res = judgerSync(config);
-            res.result.should.eql(_judger.RUNTIME_ERROR);
-            res.signal.should.eql(31);
+                    config["seccomp_rule_name"] = "general"
+                    judge(config,function(res){
+                        //with general seccomp
+                        res.result.should.eql(_judger.RUNTIME_ERROR);
+                        res.signal.should.eql(31);
 
-            // with c_cpp seccomp
-            config["seccomp_rule_name"] = "c_cpp"
-            res = judgerSync(config);
-            res.result.should.eql(_judger.RUNTIME_ERROR);
-            res.signal.should.eql(31);
-
+                        config["seccomp_rule_name"] = "c_cpp"
+                        judge(config,function(res){
+                            //with c_cpp seccomp
+                            res.result.should.eql(_judger.RUNTIME_ERROR);
+                            res.signal.should.eql(31);
+                            done()
+                        })
+                    })
+                })
+            })
         })
     })
 
     describe('test_write_file',function(){
-        it('write_file.c',function(){
+        it('write_file.c',function(done){
+            this.timeout(15000)
             var config = JSON.parse( JSON.stringify(inte_config));
             config["max_memory"] = 1024 * 1024 * 1024
-            config['exe_path'] = _compile_c( join(t_path,'write_file.c'));
-            config.output_path = config.error_path = _output_path();
 
-            // without seccomp
-            var res = judgerSync(config);
-            var out = "test";
-            res.result.should.eql(_judger.SUCCESS);
-            out.should.eql( output_content('/tmp/fffffffffffffile.txt'))
+            _compile_c( join(t_path,'write_file.c'),function(exe_path){
+                config['exe_path'] = exe_path
+                config.output_path = config.error_path = _output_path();
+                judge(config,function(res){
+                    //without seccomp
+                    var out = "test";
+                    res.result.should.eql(_judger.SUCCESS);
+                    out.should.eql( output_content('/tmp/fffffffffffffile.txt'))
 
-            // with general seccomp
-            config["seccomp_rule_name"] = "general"
-            res = judgerSync(config);
-            res.result.should.eql(_judger.RUNTIME_ERROR);
-            res.signal.should.eql(31);
+                    config["seccomp_rule_name"] = "general"
+                    judge(config,function(res){
 
-            // with c_cpp seccomp
-            config["seccomp_rule_name"] = "c_cpp"
-            res = judgerSync(config);
-            res.result.should.eql(_judger.RUNTIME_ERROR);
-            res.signal.should.eql(31);
+                        //with general seccomp
+                        res.result.should.eql(_judger.RUNTIME_ERROR);
+                        res.signal.should.eql(31);
 
+                        config["seccomp_rule_name"] = "c_cpp"
+                        judge(config,function(res){
+                            //with c_cpp seccomp
+                            res.result.should.eql(_judger.RUNTIME_ERROR);
+                            res.signal.should.eql(31);
+                            done()
+                        })
+                    })
+                })
+            })
         })
     })
 }
